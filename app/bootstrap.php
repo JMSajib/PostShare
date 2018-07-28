d1f1587a77e6d61c3986d1005a78176f1db3a89a
JMSajib <jm.sajib012@gmail.com>
2018-07-28 23:13:32 +0600

    initial commit

---
+++ app/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+  //Load Config
+  require_once 'config/config.php';
+
+  //AutoLoad Libraries
+  spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+  })
+
+?>  
