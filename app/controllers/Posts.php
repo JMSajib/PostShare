93da9e4fa54eb492159f58ceedf342e20914ae71
JMSajib <jm.sajib012@gmail.com>
2018-07-29 22:51:48 +0600

    Get Post data

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -6,10 +6,18 @@
             if(!isLoggedIn()){
                 redirect('users/login');
             }
+
+            $this->postModel = $this->model('Post');
         }
 
         public function index(){
-            $data = [];
+            
+            $posts = $this->postModel->getPosts(); 
+
+            $data = [
+                'posts' => $posts
+            ];
+
             $this->view('posts/index',$data);
         }
     }
