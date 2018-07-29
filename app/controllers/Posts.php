630495b5a3b1fac9fc30b884eaaa358d6dfc9522
JMSajib <jm.sajib012@gmail.com>
2018-07-29 23:18:27 +0600

    Added functionlaty of Post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -20,6 +20,15 @@
 
             $this->view('posts/index',$data);
         }
+
+        public function add(){
+            $data = [
+                'title' =>'',
+                'body' => ''
+            ];
+
+            $this->view('posts/add', $data);
+        }
     }
 
 ?>
